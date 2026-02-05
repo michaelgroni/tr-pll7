21a059fa458fe3975212d9a7129b8946d8a19a05
Michael Groni <mail@michael-groni.de>
2026-02-05 22:03:24 +0100

    test instructions removed from CMakeLists.txt

--- src/GPIOoutput.cpp
+++ src/GPIOoutput.cpp
@@ -8,8 +8,7 @@
 #include "beep.pio.h"
 #include "ctcss.pio.h"
 
-
-Piezo* Piezo::getInstance()
+Piezo *Piezo::getInstance()
 {
     static Piezo instance;
     return &instance;
@@ -24,7 +23,7 @@ Piezo::Piezo()
     sm_config_set_set_pins(&beepConfig, BEEP_PIN, 1);
     pio_gpio_init(BEEP_PIO, BEEP_PIN);
     pio_sm_init(BEEP_PIO, beepSm, beepOffset, &beepConfig);
-    pio_sm_set_enabled(BEEP_PIO, beepSm, false);     
+    pio_sm_set_enabled(BEEP_PIO, beepSm, false);
 }
 
 void Piezo::beepOK()
@@ -35,7 +34,6 @@ void Piezo::beepOK()
     pio_sm_set_enabled(BEEP_PIO, beepSm, false);
 }
 
-
 void Piezo::beepWriteOK()
 {
     pio_sm_set_clkdiv(BEEP_PIO, beepSm, 128);
@@ -44,11 +42,10 @@ void Piezo::beepWriteOK()
     pio_sm_set_enabled(BEEP_PIO, beepSm, false);
 }
 
-    
 void Piezo::beepError()
 {
     pio_sm_set_clkdiv(BEEP_PIO, beepSm, 255);
-    for (int i=0; i<4; i++)
+    for (int i = 0; i < 4; i++)
     {
         pio_sm_set_enabled(BEEP_PIO, beepSm, true);
         sleep_ms(50);
@@ -56,16 +53,15 @@ void Piezo::beepError()
         sleep_ms(50);
     }
 }
-    
 
-Ctcss* Ctcss::getInstance()
+Ctcss *Ctcss::getInstance()
 {
     static Ctcss instance;
     return &instance;
 }
 
 Ctcss::Ctcss()
-:sysClock(clock_get_hz(clk_sys))
+    : sysClock(clock_get_hz(clk_sys))
 {
     // setup ctcss pio
     const uint ctcssOffset = pio_add_program(CTCSS_PIO, &ctcss_program);
@@ -74,7 +70,7 @@ Ctcss::Ctcss()
     sm_config_set_set_pins(&ctcssConfig, CTCSS_PIN, 1);
     pio_gpio_init(CTCSS_PIO, CTCSS_PIN);
     pio_sm_init(CTCSS_PIO, ctcssSm, ctcssOffset, &ctcssConfig);
-    pio_sm_set_enabled(CTCSS_PIO, ctcssSm, false);  
+    pio_sm_set_enabled(CTCSS_PIO, ctcssSm, false);
 }
 
 void Ctcss::update(TrxState &trxState)
@@ -100,7 +96,7 @@ void Ctcss::setOn(const float frequency)
 {
     const float clkDiv = sysClock / (frequency * cycles);
     pio_sm_set_clkdiv(CTCSS_PIO, ctcssSm, clkDiv);
-    pio_sm_set_enabled(CTCSS_PIO, ctcssSm, true);  
+    pio_sm_set_enabled(CTCSS_PIO, ctcssSm, true);
     this->frequency = frequency;
     on = true;
 }
